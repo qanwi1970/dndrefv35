4c8ae53e24cda96b2c2a894bdd3630e7a2aaff62
Bill DeLude <qanwi1970@gmail.com>
2016-03-28 19:10:31 -0600

    First working version

---
+++ src/main/java/com/dungeonmart/ref/v35/Dndrefv35Application.java
@@ -0,0 +1,12 @@
+package com.dungeonmart.ref.v35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dndrefv35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dndrefv35Application.class, args);
+	}
+}
---
+++ src/main/java/com/dungeonmart/ref/v35/HomeController.java
@@ -0,0 +1,21 @@
+package com.dungeonmart.ref.v35;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @RequestMapping(path = "/")
+    public HttpEntity<?> getInfo() {
+        Map<String, String> infoMap = new HashMap<>();
+        infoMap.put("name", "Dungeonmart D&D v3.5 Reference");
+
+        return new ResponseEntity<Object>(infoMap, HttpStatus.OK);
+    }
+}
