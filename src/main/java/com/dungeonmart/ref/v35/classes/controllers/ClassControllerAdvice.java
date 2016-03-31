2b946e4f5c016c2c2b3949b375ee59d42591c887
Bill DeLude <qanwi1970@gmail.com>
2016-03-30 22:25:51 -0600

    error updates

--- src/main/java/com/dungeonmart/ref/v35/classes/controllers/ClassControllerAdvice.java
+++ src/main/java/com/dungeonmart/ref/v35/classes/controllers/ClassControllerAdvice.java
@@ -2,6 +2,7 @@ package com.dungeonmart.ref.v35.classes.controllers;
 
 import com.dungeonmart.ref.v35.classes.exceptions.CharacterClassNotFoundException;
 import com.dungeonmart.ref.v35.classes.exceptions.ClassLevelNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class ClassControllerAdvice {
 
@@ -16,6 +18,7 @@ public class ClassControllerAdvice {
     @ExceptionHandler(CharacterClassNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     VndErrors characterClassNotFoundExceptionHandler(CharacterClassNotFoundException ex) {
+        log.info("CharacterClassNotFound", ex);
         return new VndErrors("CharacterClassNotFound", ex.getMessage());
     }
 
@@ -23,6 +26,7 @@ public class ClassControllerAdvice {
     @ExceptionHandler(ClassLevelNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     VndErrors classLevelNotFoundExceptionHandler(ClassLevelNotFoundException ex) {
+        log.info("ClassLevelNotFound", ex);
         return new VndErrors("ClassLevelNotFound", ex.getMessage());
     }
 }
